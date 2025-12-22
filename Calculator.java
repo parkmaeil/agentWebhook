0ce3fa2b5ae6764011415254b9c7f3a2bd154170
parkmaeil <bitcocom@empas.com>
2025-12-22 13:43:06 +0900

    박매일_과제제출

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,6 @@ package com.example.assignment;
 public class Calculator {
     // [과제] 두 정수의 합을 반환하는 메서드를 완성하세요.
     public int add(int a, int b) {
-        return 0; // TODO: 여기를 올바른 로직(a + b)으로 수정하세요.
+        return a+b; 
     }
 }
